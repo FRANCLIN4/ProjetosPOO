72147cbd2e37fbcd43fbb12f1739133e418307ac
04029999 <franclin.vc22@gmail.com>
2019-03-28 20:06:35 -0300

    ProjetoPessoa

---
+++ src/ProjetoPessoa/Main.java
@@ -0,0 +1,10 @@
+package ProjetoPessoa;
+
+public class Main {
+    public  static  void  main(String args[]){
+        Pessoa p1 = new Pessoa("Pedro","2344");
+        Pessoa p2 = new Pessoa("Pedro","2344");
+
+
+    }
+}
