3ac2f9378904ff7d85444e4bc1149ca8c5d71b94
04029999 <franclin.vc22@gmail.com>
2019-03-28 20:56:05 -0300

    ProjetoPessoa 2

--- src/ProjetoPessoa/Pessoa.java
+++ src/ProjetoPessoa/Pessoa.java
@@ -1,12 +1,28 @@
 package ProjetoPessoa;
 
 public class Pessoa {
-    String nome;
-    String cpf;
+    private String nome;
+    private String cpf;
 
     Pessoa(String nome,String cpf){
         this.nome = nome;
         this.cpf = cpf;
 
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 }
